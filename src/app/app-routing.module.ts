f5d27fe6ca2362016eb0453b01be2b6798518eee
meil <ics.sora.meil@gmail.com>
2019-10-22 21:05:25 +0900

    modify scroll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: "enabled",
+      anchorScrolling: "enabled"
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
